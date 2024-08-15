699ecb47125f87f3986c8c4db815dae454b39633
czx04 <dhit2k4@gmail.com>
2024-08-15 18:48:54 +0700

    n2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <h3> Hung Du's code in codeptit <img src="https://i.gyazo.com/ad27bc12ca81e862ceb35328122757ee.png" width="22" height="25"><h3>
+
+<a href="https://www.notion.so/960f8423916643cdaa25ec7f8a549e71?v=61c9b38296004aeb84c5b6f65475316a&pvs=4">Visit My Notion Course Java</a>
--- String/j03006.java
+++ String/j03006.java
@@ -1,3 +1,28 @@
+
+import java.util.Scanner;
+
 public class j03006 {
-    
+
+    public static boolean isnicenumb(String numb){
+        int n = numb.length();
+        for(int i = 0;i<=(n-1)/2;i++){
+            int a = Character.getNumericValue(numb.charAt(i));
+            int b = Character.getNumericValue(numb.charAt(n-1-i));
+            if(a%2!=0 || a!=b) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = Integer.parseInt(scanner.nextLine());
+
+        while(t-- > 0){
+            String numb = scanner.nextLine();
+
+            if(isnicenumb(numb)) System.out.println("YES");
+            else System.out.println("NO");
+
+        }
+    }
 }
